9c8539ed2650f33a508b1948a1ec3d13afd972cf
yo <esteban.altamura@gmail.com>
2023-06-14 15:59:09 -0300

    actualizacion

--- src/componentes/chatRoom/Chat.js
+++ src/componentes/chatRoom/Chat.js
@@ -1,15 +1,22 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { webSocketConnectionContext } from "../../contexts/WebSocketConnectionProvider";
 import { AiFillCloseSquare } from "react-icons/ai";
 import "./Chat.css"
 
 export const Chat = ()=>{
 
-    const { closeConnection } = useContext(webSocketConnectionContext)
+    const { connectionstatus, closeConnection } = useContext(webSocketConnectionContext)
 
     const closeConnectionHandler = ()=>{        
         closeConnection()
     }
+
+    useEffect(()=>{
+        connectionstatus==="offline" && history("/login") 
+        connectionstatus==="online" && history("/login")
+        connectionstatus==="findingPairs" && history("/login") 
+    }     
+    ,[connectionstatus])
 
 
     return(
