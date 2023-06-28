0108d46f3b7bee856ec2ff5da6b6ab179f6fa0b5
colaboradorAltamura <xoxotrends1@gmail.com>
2023-06-28 10:07:20 -0300

    actualizacion

--- src/contexts/LastActivityTimeProvider.js
+++ src/contexts/LastActivityTimeProvider.js
@@ -9,8 +9,8 @@ export const LastActivityTimeProvider = ({children})=>{
     const [secondsFromLastActivity, setSecondsFromLastActivity] = useState(0)
     
     useEffect(()=>{
-        if(connectionstatus === "userRegistered" && secondsFromLastActivity >= 100) setConnectionStatus("disconnectionByInactivity")     
-        if(connectionstatus === "chating" && secondsFromLastActivity >= 100) setConnectionStatus("disconnectionByInactivity")
+        if(connectionstatus === "userRegistered" && secondsFromLastActivity >= 1000) setConnectionStatus("disconnectionByInactivity")     
+        if(connectionstatus === "chating" && secondsFromLastActivity >= 1000) setConnectionStatus("disconnectionByInactivity")
         
     }, [secondsFromLastActivity])
 
