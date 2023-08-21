9ad990426cd6da8b1bb205a16aae4ac6e54c16b4
estebanaltamura <esteban.altamura@gmail.com>
2023-08-21 14:46:01 -0300

    actualizacion

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -16,7 +16,7 @@ export const useChat = ()=>{
 
 	const sendMessageHandler = (e)=>{
 		e.preventDefault() 
-		e.target.focus()       
+		e.target.elements.chatInput.focus()       
 		let message = e.target.elements.chatInput.value      
 
 		if(message.length > 0){
